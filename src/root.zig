4940c20ae159b9d49cc9c403f88942d0b3af738f
Paul Horn <dev@knutwalker.engineer>
2025-10-26 00:23:07 +0200

    Add unit test for basic usage

--- src/root.zig
+++ src/root.zig
@@ -22,3 +22,32 @@ test "force analysis" {
         _ = Logger;
     }
 }
+
+test "quick example" {
+    const std = @import("std");
+
+    var output: std.ArrayListUnmanaged(u8) = .empty;
+    defer output.deinit(std.testing.allocator);
+
+    init(.{
+        .filter = .{ .level = .debug },
+        .output = .{ .writer = output.writer(std.testing.allocator).any() },
+    });
+    const logger = setupFn(.{ .enable_trace_level = true });
+
+    logger(.debug, .default, "message 1", .{});
+    logger(.info, .scope, "message {}", .{1 + 1});
+    logger(.warn, .default, "{}", .{std.zig.fmtId("message 3")});
+    logger(.err, .longer_scope, "message 4", .{});
+
+    const out = try output.toOwnedSlice(std.testing.allocator);
+    defer std.testing.allocator.free(out);
+
+    try std.testing.expectEqualStrings(
+        \\DEBUG message 1
+        \\INFO  (scope) message 2
+        \\WARN          @"message 3"
+        \\ERROR (longer_scope) message 4
+        \\
+    , out);
+}
